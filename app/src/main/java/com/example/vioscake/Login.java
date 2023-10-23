3f8e22d10a5a94e6346daf59e26edcbf300e9bfd
thedeadji <adjiprasetya07@gmail.com>
2023-10-23 07:58:54 +0700

    Workshop Mobile

---
+++ app/src/main/java/com/example/vioscake/Login.java
@@ -0,0 +1,17 @@
+package com.example.vioscake;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
---
+++ app/src/main/java/com/example/vioscake/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.vioscake;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 2000; // Durasi splash screen dalam milidetik (2 detik)
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
