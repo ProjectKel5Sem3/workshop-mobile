0c5409f4d90061f301f9c8d9fa7744ca11034d73
wunderbaumxd <akmalxenon124@gmail.com>
2023-11-10 20:42:18 +0700

    bikinpengaturanprofileseparo

--- app/src/main/java/com/example/vioscake/EditProfile.java
+++ app/src/main/java/com/example/vioscake/EditProfile.java
@@ -1,21 +1,29 @@
 package com.example.vioscake;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
+import androidx.fragment.app.Fragment;
 
 public class EditProfile extends Fragment {
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.edit_profile, container, false);
+        View view = inflater.inflate(R.layout.edit_profile, container, false);
+
+        ImageView backButton = view.findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tambahkan logika yang ingin Anda jalankan saat tombol "Back" diklik
+                requireActivity().onBackPressed();  // Ini akan kembali ke tumpukan sebelumnya atau menutup fragment jika tidak ada tumpukan.
+            }
+        });
+
+        return view;
     }
 }
