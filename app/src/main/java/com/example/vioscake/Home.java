2b54d1d9aa1b6ab0f704a7c6870e38c138501afa
wunderbaumxd <akmalxenon124@gmail.com>
2023-11-10 19:15:23 +0700

    homejadi

--- app/src/main/java/com/example/vioscake/Home.java
+++ app/src/main/java/com/example/vioscake/Home.java
@@ -3,17 +3,49 @@ package com.example.vioscake;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class Home extends Fragment {
+
+    RecyclerView recyclerView;
+    AdapterRecyclerView adapterRecyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<ItemModel> data;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
 
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.recycleproduk);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new GridLayoutManager(requireContext(), 2);
+        recyclerView.setLayoutManager(layoutManager);
+
+        data = new ArrayList<>();
+        for (int i = 0; i < KueItem.NamaKue.length; i++){
+            data.add(new ItemModel(
+                    KueItem.NamaKue[i],
+                    KueItem.HargaKue[i],
+                    KueItem.LogoKue[i]
+            ));
+        }
+
+
+        adapterRecyclerView = new AdapterRecyclerView(data);
+        recyclerView.setAdapter(adapterRecyclerView);
+
+
+        return view;
     }
+
 }
---
+++ app/src/main/java/com/example/vioscake/KueItem.java
@@ -0,0 +1,23 @@
+package com.example.vioscake;
+
+import androidx.fragment.app.Fragment;
+
+public class KueItem extends Fragment {
+
+    static int[] LogoKue = {
+            R.drawable.kuesatu, R.drawable.kuedua, R.drawable.kuetiga,
+            R.drawable.kueempat, R.drawable.kuelima, R.drawable.kueenam,
+            R.drawable.kuetujuh
+
+    };
+
+    static String[] NamaKue = {
+            "Kue_Satu", "Kue_Dua", "Kue_Tiga",
+            "Kue_Empat", "Kue_Lima", "Kue_Enam", "Kue_Tujuh"
+    };
+
+    static String[] HargaKue = {
+            "Harga_Satu", "Harga_Dua", "Harga_Tiga",
+            "Harga_Empat", "Harga_Lima", "Harga_Enam", "Harga_Tujuh"
+    };
+}
