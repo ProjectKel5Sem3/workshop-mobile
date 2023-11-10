d0efa8d6f379e3b4a635c1d7ca19f12624d8c5ea
wunderbaumxd <akmalxenon124@gmail.com>
2023-11-10 20:32:23 +0700

    teserror

--- app/src/main/java/com/example/vioscake/NavbarFragment.java
+++ app/src/main/java/com/example/vioscake/NavbarFragment.java
@@ -1,18 +1,16 @@
 package com.example.vioscake;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.view.MenuItem;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NavbarFragment extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +20,14 @@ public class NavbarFragment extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigation_bar);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment fragment = null;
+
             if (item.getItemId() == R.id.navigation_home) {
                 fragment = new Home();
             } else if (item.getItemId() == R.id.navigation_katalog) {
@@ -35,9 +37,30 @@ public class NavbarFragment extends AppCompatActivity {
             } else if (item.getItemId() == R.id.navigation_account) {
                 fragment = new Profile();
             }
-            return loadFragment(fragment);
+
+            if (fragment != null) {
+                for (int i = 0; i < bottomNavigationView.getMenu().size(); i++) {
+                    MenuItem otherItem = bottomNavigationView.getMenu().getItem(i);
+                    if (otherItem.getItemId() == item.getItemId()) {
+                        // Highlight the selected item
+                        otherItem.setChecked(true);
+                        otherItem.setIconTintList(getResources().getColorStateList(R.color.selected_icon_color));
+                    } else {
+                        // Unhighlight other items
+                        otherItem.setChecked(false);
+                        otherItem.setIconTintList(getResources().getColorStateList(R.color.unselected_icon_color));
+                    }
+                }
+
+                // Load the selected fragment
+                loadFragment(fragment);
+                return true;  // Keluar dari metode setelah menangani item yang dipilih
+            }
+
+            return false;
         }
     };
+
     private boolean loadFragment(Fragment fragment) {
         if (fragment == null) {
             return false;
--- app/src/main/java/com/example/vioscake/Profile.java
+++ app/src/main/java/com/example/vioscake/Profile.java
@@ -7,12 +7,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Profile extends Fragment {
+public class Profile extends Fragment implements View.OnClickListener {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Menambahkan onClickListener ke elemen dengan ID editProfile
+        TextView editProfileButton = view.findViewById(R.id.Pengaturan);
+        editProfileButton.setOnClickListener(this);
+
+        return view;
+    }
+
+    // Metode yang akan dipanggil ketika elemen dengan ID editProfile diklik
+
+
+    // Metode untuk memunculkan halaman edit_profile.xml
+    private void loadEditProfilePage() {
+        // Lakukan tindakan untuk memunculkan halaman edit_profile.xml
+        // Misalnya, menggunakan FragmentTransaction untuk mengganti fragment
+        EditProfile editProfileFragment = new EditProfile();
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, editProfileFragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.Pengaturan) {
+            // Munculkan halaman edit_profile.xml atau lakukan tindakan yang sesuai
+            loadEditProfilePage();
+        }
     }
 }
