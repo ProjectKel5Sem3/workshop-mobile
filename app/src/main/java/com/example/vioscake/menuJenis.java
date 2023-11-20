050d801c0f7e402804e6ba79ee31df3ee7517665
wunderbaumxd <akmalxenon124@gmail.com>
2023-11-20 09:18:06 +0700

    lelah

--- app/src/main/java/com/example/vioscake/menuJenis.java
+++ app/src/main/java/com/example/vioscake/menuJenis.java
@@ -1,4 +1,4 @@
-package com.example.vioscake.EntryPackage;
+package com.example.vioscake;
 
 import android.os.Bundle;
 
@@ -8,14 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.vioscake.R;
 
-
-public class menuMain extends Fragment {
+public class menuJenis extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_main, container, false);
+        return inflater.inflate(R.layout.fragment_menu_jenis, container, false);
     }
 }
